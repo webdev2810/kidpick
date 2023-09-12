4c361c4ab895c7ed418c0a1d0a1c6982f80bbec5
webdev2810 <138306196+webdev2810@users.noreply.github.com>
2023-09-12 17:33:59 +0530

    Fix: bcrypt error fixed.

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use('/uploads', express.static('uploads'))
 const PORT = process.env.PORT || 3000
 
 //cors
-const cors = require("cors");
-app.use(cors());
+// const cors = require("cors");
+// app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
